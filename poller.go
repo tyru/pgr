2188a407a6861a4813d5c1cb1890e4ccced975b1
tyru <tyru.exe@gmail.com>
2018-01-10 21:24:46 +0900

    Poller: handle errors

--- poller.go
+++ poller.go
@@ -2,6 +2,7 @@ package pgr
 
 import (
 	"context"
+	"errors"
 	"os"
 	"sync"
 	"time"
@@ -28,29 +29,40 @@ func (p *Poller) Add(bars ...*Bar) *Poller {
 	return p
 }
 
-func (p *Poller) Show(ctx context.Context) {
+var ErrCanceled = errors.New("canceled by context")
+
+func (p *Poller) Show(ctx context.Context) error {
 	termSave()
 	tick := time.NewTicker(p.duration).C
 	for {
 		select {
 		case <-ctx.Done():
-			return
+			return ErrCanceled
 		case <-tick:
 			termRestore()
-			if p.poll() {
-				return
+			if err := p.poll(); err == nil {
+				return nil
+			} else if err != errUnfinished {
+				return err
 			}
 		}
 	}
 }
 
-func (p *Poller) poll() bool {
-	doneAll := true
+var errUnfinished = errors.New("not finished yet")
+
+// poll() returns nil error if all bars are finished.
+func (p *Poller) poll() (err error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
 	for i := range p.bars {
-		p.bars[i].template.Execute(os.Stdout, p.bars[i])
+		if err := p.bars[i].template.Execute(os.Stdout, p.bars[i]); err != nil {
+			return err
+		}
 		if p.bars[i].Current() < p.bars[i].Total() {
-			doneAll = false
+			err = errUnfinished
 		}
 	}
-	return doneAll
+	return err
 }
