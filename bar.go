cc7f901b09f7d680e9e26245e5de060a31a72e3f
tyru <tyru.exe@gmail.com>
2018-01-12 01:19:18 +0900

    Add percent,bar functions

--- bar.go
+++ bar.go
@@ -2,6 +2,7 @@ package pgr
 
 import (
 	"strconv"
+	"strings"
 	"sync/atomic"
 	"text/template"
 )
@@ -71,6 +72,45 @@ var funcMaps = template.FuncMap{
 	"total": func(value interface{}, args ...string) string {
 		if bar, ok := value.(*Bar); ok {
 			return strconv.FormatInt(bar.Total(), 10)
+		}
+		return ""
+	},
+	"percent": func(value interface{}) string {
+		if bar, ok := value.(*Bar); ok {
+			percent := int(float64(bar.Current()) / float64(bar.Total()) * 100)
+			return strconv.Itoa(percent) + "%"
+		}
+		return ""
+	},
+	"bar": func(value interface{}, prefix, complete, current, incomplete, suffix string, opt ...int) string {
+		if bar, ok := value.(*Bar); ok {
+			col := 80
+			if len(opt) > 0 {
+				col = opt[0]
+			}
+			ccWidth := col - len(prefix+current+suffix)
+			if ccWidth <= 0 {
+				return "" // no space
+			}
+
+			// len(complete) = n, len(uncomplete) = m
+			//
+			//   n : n + m = bar.Current() : bar.Total()
+			//   (n + m) * bar.Current() = n * bar.Total()
+			//
+			// col - len(prefix+current+suffix) = ccWidth
+			// ccWidth = n + m
+			//
+			//   ccWidth * bar.Current() = n * bar.Total()
+			//   n = ccWidth * bar.Current() / bar.Total()
+
+			completeCount := int(float64(ccWidth) * float64(bar.Current()) / float64(bar.Total()))
+			incompleteCount := ccWidth - completeCount
+			return prefix +
+				strings.Repeat(complete, completeCount) +
+				current +
+				strings.Repeat(incomplete, incompleteCount) +
+				suffix
 		}
 		return ""
 	},
