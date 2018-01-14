066fc664a314fdacc3063f6b2e76034e2cdd4a07
tyru <tyru.exe@gmail.com>
2018-01-15 00:10:56 +0900

    Add _example/rikka.go

---
+++ _example/rikka.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/tyru/pgr"
+)
+
+func main() {
+	bar := pgr.NewBarFunc(100, rikka())
+	poller := pgr.NewPoller(100 * time.Millisecond).Add(bar)
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		time.Sleep(3 * time.Second)
+		cancel()
+	}()
+	poller.Show(ctx)
+}
+
+func rikka() pgr.FormatFunc {
+	i := 0
+	return func(*pgr.Bar) string {
+		i++
+		switch i % 4 {
+		case 0:
+			return "(b 回ω・)b"
+		case 1:
+			return "(σ回ω・)σ"
+		case 2:
+			return "(q 回ω・)q"
+		default:
+			return "(ρ回ω・)ρ"
+		}
+	}
+}
